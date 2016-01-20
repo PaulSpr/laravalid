080e44bff6239f3601430591413390427d34c28f
Paul <paul@interactivestudios.nl>
2016-01-20 22:54:49 +0100

    Added initial set methods to create complete sets of labels and inputs

--- src/Bllim/Laravalid/FormBuilder.php
+++ src/Bllim/Laravalid/FormBuilder.php
@@ -20,10 +20,13 @@
  * @version    0.9
  */
 use Lang;
+use App;
 
 class FormBuilder extends \Collective\Html\FormBuilder {
 
 	protected $converter;
+
+	public static $requiredLabel = ' <span class="is-form-required-label">*</span>';
 
 	public function __construct(\Collective\Html\HtmlBuilder $html, \Illuminate\Routing\UrlGenerator $url, $csrfToken, Converter\Base\Converter $converter, \Illuminate\Contracts\View\Factory $view)
 	{
@@ -107,6 +110,104 @@ class FormBuilder extends \Collective\Html\FormBuilder {
 		$options = $this->converter->convert(Helper::getFormAttribute($name)) + $options;
 		return parent::input($type, $name, $value, $options);
 	}
+
+
+
+
+	public function postalcode($name, $value = null, $options = [])
+	{
+		if(isset($options['class'])){
+			$options['class'] .= ' postalcodeNL';
+		}
+		else{
+			$options['class'] = 'postalcodeNL';
+		}
+		$options = $this->converter->convert(Helper::getFormAttribute($name)) + $options;
+		return parent::text($name, $value, $options);
+	}
+
+
+	public function makeSet( $label, $input ){
+		return '
+		<div class="row is-form-row">
+			<div class="is-form-label-holder">
+				'.$label.'
+			</div>
+			<div class="is-form-widget-holder">
+				'.$input.'
+			</div>
+        </div>';
+	}
+
+
+	public function textSet($name, $options = [], $value = null){
+		return self::inputSet('text', $name, $value, $options);
+	}
+	public function passwordSet($name, $options = [], $value = null){
+		return self::inputSet('password', $name, $value, $options);
+	}
+	public function emailSet($name, $options = [], $value = null){
+		return self::inputSet('email', $name, $value, $options);
+	}
+	public function telSet($name, $options = [], $value = null){
+		return self::inputSet('tel', $name, $value, $options);
+	}
+	public function numberSet($name, $options = [], $value = null){
+		return self::inputSet('number', $name, $value, $options);
+	}
+
+	public function dateSet($name, $options = [], $value = null){
+		return self::inputSet('date', $name, $value, $options);
+	}
+	public function datetimeSet($name, $options = [], $value = null){
+		return self::inputSet('datetime', $name, $value, $options);
+	}
+	public function datetimeLocalSet($name, $options = [], $value = null){
+		return self::inputSet('datetime-local', $name, $value, $options);
+	}
+
+	public function timeSet($name, $options = [], $value = null){
+		return self::inputSet('time', $name, $value, $options);
+	}
+	public function urlSet($name, $options = [], $value = null){
+		return self::inputSet('url', $name, $value, $options);
+	}
+	public function fileSet($name, $options = [], $value = null){
+		return self::inputSet('file', $name, $value, $options);
+	}
+
+	public function postalcodeSet($name, $options = [], $value = null){
+		$customInput = self::postalcode($name, $value, $options);
+		return self::inputSet('text', $name, $value, $options, $customInput);
+	}
+
+
+	public function inputSet($type, $name, $value = null, $options = [], $customInput = null){
+		$options = $this->converter->convert(Helper::getFormAttribute($name)) + $options;
+
+		$labelText = isset($options['label']) ? $options['label'] : null;
+		$requiredLabel = isset($options['data-rule-required']) ? self::$requiredLabel : '';
+
+		$label = self::label($name, $labelText, [], $requiredLabel);
+		$input = isset($customInput) ? $customInput : parent::input($type, $name, $value, $options);
+		return self::makeSet($label, $input);
+	}
+
+
+
+	/**
+	 * @see Illuminate\Html\FormBuilder
+	 */
+	public function label($name, $value = null, $options = [], $requiredLabel = ''){
+		$this->labels[] = $name;
+
+		$options = $this->html->attributes($options);
+
+		$value = e($this->formatLabel($name, $value));
+
+		return $this->toHtmlString('<label for="' . $name . '"' . $options . ' class="is-form-label">' . $value . $requiredLabel . '</label>');
+	}
+
 
 	/**
 	 * @see Illuminate\Html\FormBuilder
